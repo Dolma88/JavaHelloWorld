9375f1a56331c2096aacf6354656b1f16cb3d8f0
dolma88 <sam.atc@gmail.com>
2019-11-26 19:40:32 +0300

    Обновлен файл gitignore

---
+++ src/com/levelp/jb2/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.levelp.jb2;
+
+// Created by Dolma88 first project 26.11.2019
+
+public class HelloWorld {
+    public static void main {String[] args {
+    System.out.println("Hello World!!! ");
+    }
+}
